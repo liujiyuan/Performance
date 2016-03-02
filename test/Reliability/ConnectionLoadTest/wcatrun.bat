50547116bd5f1e0d730252f6ca0eff49a28b8071
Sajay <sajaya@microsoft.com>
2016-03-01 17:45:42 -0800

    Revert "Added test files for connection load test."
    This reverts commit 78df0629ea1fe22057a6e9411fea1ee4d29f6775.

--- test/Reliability/ConnectionLoadTest/wcatrun.bat
+++
@@ -1,6 +0,0 @@
-taskkill /f /t /im wcctl
-taskkill /f /t /im wcctl.exe
-taskkill /f /t /im wcclient.exe
-taskkill /f /t /im wcclient
-start .\wcclient.exe localhost -b
-.\wcctl.exe -t .\scenario.ubr -f .\settings.ubr -p 80 -c 1 -v 10000 -o .\out.xml -x
