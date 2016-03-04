d613b6b881ad3cbae74a512c964cd0cd50f8c8c5
Ryan Nowak <nowakra@gmail.com>
2016-03-04 14:36:15 -0800

    Updates to BasicViews
    - Remove DNX
    - Add EF + LocalDB
    - Make Layout more like our template
    - Add some validation attributes

--- testapp/BasicViews/Controllers/HomeController.cs
+++ testapp/BasicViews/Controllers/HomeController.cs
@@ -1,21 +1,55 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BasicViews.Controllers
 {
     public class HomeController : Controller
     {
-        [ValidateAntiForgeryToken]
-        public IActionResult Index(Person person)
+        private readonly BasicViewsContext _context;
+
+        public HomeController(BasicViewsContext context)
         {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Index(Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(person);
+                await _context.SaveChangesAsync();
+            }
+
             return View(person);
         }
 
-        [ValidateAntiForgeryToken]
-        public IActionResult HtmlHelpers(Person person)
+        [HttpGet]
+        public IActionResult HtmlHelpers()
         {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> HtmlHelpers(Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(person);
+                await _context.SaveChangesAsync();
+            }
+
             return View(person);
         }
     }
--- testapp/BasicViews/Startup.cs
+++ testapp/BasicViews/Startup.cs
@@ -4,19 +4,43 @@
 using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.PlatformAbstractions;
 
 namespace BasicViews
 {
     public class Startup
     {
+        public Startup(IHostingEnvironment hosting)
+        {
+            Configuration = 
+                new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .SetBasePath(PlatformServices.Default.Application.ApplicationBasePath)
+                .Build();
+        }
+
+        public IConfiguration Configuration { get; }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            var connectionString = Configuration["Data:DefaultConnection:ConnectionString"];
+            services
+                .AddEntityFramework()
+                .AddSqlServer()
+                .AddDbContext<BasicViewsContext>(c => c.UseSqlServer(connectionString));
+
             services.AddMvc();
         }
 
         public void Configure(IApplicationBuilder app)
         {
+            CreateDatabase(app.ApplicationServices);
+
+            app.UseIISPlatformHandler();
+
             app.Use(next => async (context) =>
             {
                 try
@@ -32,6 +56,16 @@ namespace BasicViews
 
             app.UseStaticFiles();
             app.UseMvcWithDefaultRoute();
+        }
+
+        private void CreateDatabase(IServiceProvider services)
+        {
+            using (var serviceScope = services.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+                var dbContext = services.GetRequiredService<BasicViewsContext>();
+                dbContext.Database.EnsureDeleted();
+                dbContext.Database.EnsureCreated();
+            }
         }
 
         public static void Main(string[] args)
