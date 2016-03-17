00f0d6a768b119b9163f220dcad236298093f8df
Sajay Antony <sajaya@microsoft.com>
2016-03-17 11:50:36 -0700

    Cleanup installdotnet for coldstart.

--- test/ColdStart/InstallDotnet.ps1
+++ test/ColdStart/InstallDotnet.ps1
@@ -1,15 +1,25 @@
-if (! (Test-Path variable:global:workspace)) {
-    Write-Error "Workspace dir is not set"
+[CmdletBinding()]
+Param(
+   [Parameter(Mandatory=$false)]
+   [string]$outputDirectory = "."
+)
+
+if (! (Test-Path $outputDirectory)) {
+    Write-Error ("Output dir '" + $outputDirectory + "' is incorrect.")
     Exit -1
 }
 
-$installExe = Join-Path $global:Workspace "dotnet-win-x64.latest.exe"
-Invoke-WebRequest -Uri https://dotnetcli.blob.core.windows.net/dotnet/beta/Installers/Latest/dotnet-win-x64.latest.exe -OutFile $installExe
+$installExe = Join-Path $outputDirectory "dotnet-win-x64.latest.exe"
+if(Test-path $installExe) {
+    Write-Warning ($installExe + " already exists and will be replaced.")
+}
 
- &($installExe) -s
+$url = "https://dotnetcli.blob.core.windows.net/dotnet/beta/Installers/Latest/dotnet-win-x64.latest.exe"
 
-## the following is a hack to resolve a coreclr path probing issue
-Start-Sleep -s 20
-$sourceDir = [System.IO.Path]::Combine($env:ProgramFiles, "dotnet", "bin")
-$destDir = [System.IO.Path]::Combine($env:LocalAppData, "dotnet",  "runtime", "coreclr")
-Copy-Item $sourceDir -Destination $destDir -Force -Recurse
+# Bits is fast but if its failing or not avaialble then switch to Invoke-WebRequest if we can't enable it on the box. 
+# Invoke-WebRequest -Uri $url  -OutFile $installExe
+Import-Module BitsTransfer
+Start-BitsTransfer -Source $url -Destination $installExe
+
+Unblock-File $installExe
+start-process -Wait $installExe
