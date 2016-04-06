5771f642886d61da8f71e9602928468dd62a2c89
Ryan Nowak <nowakra@gmail.com>
2016-04-06 12:33:42 -0700

    Update HelloWorldMvc

---
+++ testapp/HelloWorldMvc/Controllers/PlaintextController.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWorldMvc
+{
+    public class PlaintextController : Controller
+    {
+        private static readonly byte[] _bytes = Encoding.UTF8.GetBytes("Hello, World!");
+
+        public Task Index()
+        {
+            Response.StatusCode = StatusCodes.Status200OK;
+            Response.ContentType = "text/plain";
+            Response.ContentLength = _bytes.Length;
+            return Response.Body.WriteAsync(_bytes, 0, _bytes.Length);
+        }
+    }
+}
--- testapp/HelloWorldMvc/loadtest.ps1
+++ testapp/HelloWorldMvc/loadtest.ps1
@@ -1,4 +1,19 @@
-param([int] $iterations = 3000)
+param(
+    [int] $iterations = 3000,
+    [int] $rps = 250,
+    [string][ValidateSet("plaintext", "html")] $variation = "plaintext")
 
-$url = "http://127.0.0.1:5000/"
-& loadtest -k -n $iterations -c 16 --rps 100 $url
+if ($variation -eq "plaintext")
+{
+    $url = "http://127.0.0.1:5000/Plaintext"
+}
+elseif ($variation -eq "html")
+{
+    $url = "http://127.0.0.1:5000/Home"
+}
+
+Write-Host -ForegroundColor Green Beginning workload
+Write-Host "`& loadtest -k -n $iterations -c 32 --rps $rps $url"
+Write-Host
+
+& loadtest -k -n $iterations -c 32 --rps $rps $url
